8584870128f9405eb4095d6c7ed86e81d262c261
PalashHalder <halder.palash08@gmail.com>
2022-08-11 15:09:58 +0600

    QuestionActivity updated

--- app/src/main/java/bd/gov/ansarvdpbank/avubinfo/QuestionActivity.java
+++ app/src/main/java/bd/gov/ansarvdpbank/avubinfo/QuestionActivity.java
@@ -3,14 +3,68 @@ package bd.gov.ansarvdpbank.avubinfo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 
 
 public class QuestionActivity extends AppCompatActivity {
 
+    WebView webView;
+
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);       // To enable JavaScript of the website
+
+
+        webView.loadUrl("https://docs.google.com/document/d/11NlP8oU5MGZVtZNAiBsz5PE_ooupLux0XpSbfHz6Csw/edit?usp=sharing");        //  URL of the website
+
+        webView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+                super.onProgressChanged(view, newProgress);
+
+                progressBar.setProgress(newProgress);
+            }
+        });
+
+    }
+    // This section is for working back button
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else{
+            super.onBackPressed();
+        }
     }
 }
